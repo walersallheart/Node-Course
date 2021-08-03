ee6278979781e572a126ca7062c113f7de152a73
walersallheart <53576683+walersallheart@users.noreply.github.com>
2021-08-03 15:01:31 -0700

    30 Sending Responses.

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    //process.exit(); //close the node server
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`
+        <html>
+            <head><title>My First Page</title></head>
+            <body><h1>Hello world</h1></body>
+        </html>
+    `);
+    res.end();
 });
 
 server.listen(3000)
