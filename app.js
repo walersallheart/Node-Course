c488fbe571cc717dc4ae3996dcbdce610fef26cb
walersallheart <53576683+walersallheart@users.noreply.github.com>
2021-08-03 21:27:14 -0700

    34 Parsing Request Bodies.

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    //process.exit(); //close the node server
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write(`<html>
+                <head><title>Enter Message</title></head>
+                <body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>
+        </html>`);
+
+        return res.end();
+    }
+
+    if (url === '/message' && method == "POST"){
+        const body = [];
+
+        req.on('data', (chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+
+            fs.writeFileSync('message.txt', message);
+        });
+
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
 
     res.setHeader('Content-Type', 'text/html');
     res.write(`
