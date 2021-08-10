aaffdc019c5387b87838392d66fa2dbda9fc292b
walersallheart <53576683+walersallheart@users.noreply.github.com>
2021-08-10 10:00:12 -0700

    74 Styling our Pages.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = new express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const rootDir = require('./util/path');
 
 //allows us to parse incoming get/post requests
 app.use(bodyParser.urlencoded({extended:false}));
@@ -16,7 +17,7 @@ app.use(shopRoutes);
 
 //having no path works for anything not found in any other path
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, '..', 'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
