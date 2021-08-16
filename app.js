57368ed590f62b0b5bc5b1f5629d96c5846a714d
walersallheart <53576683+walersallheart@users.noreply.github.com>
2021-08-16 12:32:07 -0700

    88 Working With Handlebars.

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs({
+    extname: "hbs",
+    defaultLayout: "",
+    layoutsDir: "",
+}));
+
+app.set('view engine', 'hbs'); //has to match the name we gave it on the previous line
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -17,7 +24,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle:'Page Not Found'})
 });
 
 app.listen(3000);
