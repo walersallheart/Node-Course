b9ac04d29c587f625fa96a112784240010c0550a
walersallheart <53576683+walersallheart@users.noreply.github.com>
2021-08-26 12:52:56 -0700

    123 Adding a Cart Model.

---
+++ models/cart.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+);
+
+module.exports = class Cart {
+    static addProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContents) => {
+            let cart = {products:[], totalPrice: 0};
+
+            if (!err) {
+                cart = JSON.parse(fileContents);
+            }
+
+            const existingProductIndex = cart.products.findIndex(prod => prod.id == id);
+            const existingProduct = cart.products[existingProductIndex];
+
+            let updatedProduct;
+
+            if (existingProduct) {
+                updatedProduct = { ...existingProduct }; //add all properties of the existing product to the updated product
+                updatedProduct.qty++;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
+            } else {
+                updatedProduct = { id: id, qty: 1 }
+                cart.products = [...cart.products, updatedProduct];
+            }
+
+            cart.totalPrice += Number(productPrice);
+
+            fs.writeFile(p, JSON.stringify(cart), err => {
+                console.log(err);
+            });
+        });
+    }
+}
