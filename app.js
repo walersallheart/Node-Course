4bf49fca4e1acf9c6a01d4fbcaf62989cb36140f
walersallheart <53576683+walersallheart@users.noreply.github.com>
2021-08-31 13:38:56 -0700

    142 Retrieving Data.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -12,6 +13,16 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products')
+    .then(result =>{
+        console.clear();
+        console.log(result[0]);
+        console.log(result[1]);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
