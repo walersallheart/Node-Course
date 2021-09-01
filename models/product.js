d914b2bb176b88f795727502afd75e10c2b65575
walersallheart <53576683+walersallheart@users.noreply.github.com>
2021-09-01 11:00:40 -0700

    145 Inserting Data Into the Database.

--- models/product.js
+++ models/product.js
@@ -11,7 +11,9 @@ module.exports = class Product {
   }
 
   save() {
-    
+    return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)', //this question mark method handles sql injection
+      [this.title, this.price, this.imageUrl, this.description]
+    );
   }
 
   static deleteById(id) {
