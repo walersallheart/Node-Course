10b72ce3a81039da850f0e798be7b41ec1c88b5f
walersallheart <53576683+walersallheart@users.noreply.github.com>
2021-09-01 13:21:47 -0700

    151 Connecting to the Database.

--- util/database.js
+++ util/database.js
@@ -1,3 +1,10 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'b078JQrwJ72', {dialect: 'mysql', host:'localhost'});
+
+module.exports = sequelize;
+
+/*
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
@@ -7,4 +14,4 @@ const pool = mysql.createPool({
     password: 'b078JQrwJ72'
 });
 
-module.exports = pool.promise();
+module.exports = pool.promise();*/
