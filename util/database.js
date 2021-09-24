3f19605087e3b43b7eb172618af1ab7c24304e63
finalmegaman NA <finalmegaman@gmail.com>
2021-09-24 11:51:44 -0700

    185 Understanding the MongoDB Compass.

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const MongoClient = mongodb.MongoClient;
 let _db;
 
 const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://willy:G33Kr0uJhV7xYb5T@cluster0.7uqja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  MongoClient.connect('mongodb+srv://willy:G33Kr0uJhV7xYb5T@cluster0.7uqja.mongodb.net/shop?retryWrites=true&w=majority')
     .then(client => {
       console.log('Connected!');
       _db = client.db();
