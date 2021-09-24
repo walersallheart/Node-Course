6e691f63620077daacf66f866b12af8363c2c8c1
finalmegaman NA <finalmegaman@gmail.com>
2021-09-24 11:37:44 -0700

    182 Finishing the Database Connection.

--- util/database.js
+++ util/database.js
@@ -1,15 +1,26 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect('mongodb+srv://willy:G33Kr0uJhV7xYb5T@cluster0.7uqja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err)
     });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+
+  throw 'No Database found!';
+}
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
