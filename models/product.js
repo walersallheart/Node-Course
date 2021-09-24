4869005bd45a19ac1b412690c23b3ebe9f8aaa8e
finalmegaman NA <finalmegaman@gmail.com>
2021-09-24 11:46:50 -0700

    184 Creating Products.

--- models/product.js
+++ models/product.js
@@ -11,13 +11,14 @@ class Product {
   save() {
     const db = getDb();
 
-    db.collection('products').insertOne(this)
-    .then(result => {
-      console.log(result);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    return db.collection('products')
+      .insertOne(this)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 
