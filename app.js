29c3f903052b11874a895eeae4c59fc08b030386
finalmegaman NA <finalmegaman@gmail.com>
2021-09-30 10:28:38 -0700

    218 Adding and Using a User Model.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-//const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -15,17 +15,19 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
+app.use((req, res, next) => {
+  User.findById('6155f333f345b9426a66727d')
     .then(user => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
-});*/
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -37,6 +39,21 @@ mongoose
     'mongodb+srv://willy:G33Kr0uJhV7xYb5T@cluster0.7uqja.mongodb.net/shop?retryWrites=true&w=majority'
   )
   .then(result => {
+    User.findOne()
+      .then(user => {
+        if (!user) {
+          const user = new User({
+            name: "Batman",
+            email: "Batman@justiceleague.org",
+            cart: {
+              items: []
+            }
+          });
+
+          user.save();
+        }
+      })
+
     app.listen(3000);
   })
   .catch(err => {
