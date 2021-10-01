333eedcc29420699bbd04773d0e1083451c0094a
finalmegaman NA <finalmegaman@gmail.com>
2021-10-01 11:48:39 -0700

    240 Using the Session Middleware.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,7 @@
 exports.getLogin = (req, res, next) => {
+  console.log(req.session);
+  console.log(req.session.isLoggedIn);
+
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -7,7 +10,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true');
-
+  req.session.isLoggedIn = true;
   res.redirect('/');
 };
