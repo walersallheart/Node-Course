ddae09fc1a20419554645175bfac87508fdc5582
finalmegaman NA <finalmegaman@gmail.com>
2021-10-06 11:46:53 -0700

    255 Encrypting Passwords.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -38,9 +39,13 @@ exports.postSignup = (req, res, next) => {
       if (userDoc) {
         return res.redirect('/signup');
       }
+
+      return bcrypt.hash(password, 12);
+    })
+    .then(hashedPassword => {
       const user = new User({
         email: email,
-        password: password,
+        password: hashedPassword,
         cart: { items: [] }
       });
       return user.save();
