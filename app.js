0f224afbb037e2cce1926a504963f5d03003244b
finalmegaman NA <finalmegaman@gmail.com>
2021-10-14 11:40:47 -0700

    323 Filtering Files by Mimetype.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const MongoDBStore = require('connect('***PASSWORD REMOVED***');;
 const csrf = require('csurf');
 const flash = require('connect-flash');
+const multer = require('multer');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -20,6 +21,23 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
+const fileStorage = multer.diskStorage({
+  destination:(req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename:(req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -28,6 +46,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image')); //single means we'll only get a single file
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
