d23e053b9b89278e2c76dc14322f9adc8cc8dec9
finalmegaman NA <finalmegaman@gmail.com>
2021-10-14 15:25:17 -0700

    326 Serving Images Statically.

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image')); //single means we'll only get a single file
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
   session({
     secret: 'my secret',
