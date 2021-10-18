96ec12750c0b6fdb46e28d672f51f29e54abae5d
finalmegaman NA <finalmegaman@gmail.com>
2021-10-18 13:12:54 -0700

    348 Adding Client Side JS Code.

---
+++ public/js/admin.js
@@ -0,0 +1,6 @@
+const deleteProduct = (btn) => {
+    const productId = btn.parentNode.querySelector('[name=productId]').value;
+    const _csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+
+    
+}
