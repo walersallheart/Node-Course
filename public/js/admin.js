9963026770ed93d1024171c4d2c1be77d2dd0af0
finalmegaman NA <finalmegaman@gmail.com>
2021-10-25 13:55:43 -0700

    Clean cut at 421.

--- public/js/admin.js
+++
@@ -1,6 +0,0 @@
-const deleteProduct = (btn) => {
-    const prodId = btn.parentNode.querySelector('[name=productId]').value;
-    const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
-
-
-}
