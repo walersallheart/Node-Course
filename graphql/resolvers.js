7ca70ad6d4be60d25665baad20c4840a81da2859
finalmegaman NA <finalmegaman@gmail.com>
2021-10-27 11:56:16 -0700

    428 Adding Login Functionality.

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     createUser: async function({userInput}, req) {
@@ -41,5 +42,29 @@ module.exports = {
         return {
             ...createdUser._doc, _id: createdUser._id.toString()
         };
+    },
+    login: async function ({ email, password }) {
+        const user = await User.findOne({email:email});
+
+        if (!user) {
+            const error = new Error('User not found');
+            error.code = 401;
+            throw error;
+        }
+
+        const isEqual = await bcrypt.compare(password, user.password);
+
+        if (!isEqual) {
+            const error = new Error('Password is incorrect');
+            error.code = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({
+            userId:user._id.toString(),
+            email: user.email
+        }, 'somesupersecretsecret', { expiresIn: '1h' });
+
+        return { token:token, userId: user._id.toString() }
     }
 }
