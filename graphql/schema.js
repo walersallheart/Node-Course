a9e1740e90fdc81d867693c022bf9e26b526c8b1
finalmegaman NA <finalmegaman@gmail.com>
2021-10-27 11:18:02 -0700

    424 Adding a Mutation Resolver & GraphiQL.

--- graphql/schema.js
+++ graphql/schema.js
@@ -25,12 +25,17 @@ module.exports = buildSchema(`
         name:String!
         password:String!
     }
+    
+    type RootQuery{
+        hello:String
+    }
 
     type RootMutation {
         createUser(userInput: UserInputData): User!
     }
 
     schema {
+        query: RootQuery
         mutation: RootMutation
     }
 `)
