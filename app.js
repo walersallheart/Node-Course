b11fc29b6a7293211ac01ff4783b2924440ac376
finalmegaman NA <finalmegaman@gmail.com>
2021-10-29 11:52:56 -0700

    453 Compressing Assets.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
 const helmet = require('helmet');
+const compression = require('compression');
 
 const errorController = require('./controllers/error');
 const shopController = require('./controllers/shop');
@@ -54,6 +55,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(helmet());
+app.use(compression());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
